6e51a0887efb1e283804d3289411917f5925ad4f
nl253 <nl253@kent.ac.uk>
2017-06-20 13:26:55 +0100

    Update.

--- .shells/variables.sh
+++ .shells/variables.sh
@@ -25,7 +25,7 @@ add-to-path(){
 
 export PATH="/usr/local/sbin:/bin:/usr/local/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/usr/bin:"
 
-add-to-path ~/{.local,.cabal,.cargo,.gem,go,anaconda3}/bin  ~/{.bin,.config/composer/vendor/bin} /usr/local/go/bin ~/.gem/ruby/*/bin
+add-to-path ~/{.local,.cabal,.cargo,.gem,go,anaconda3}/bin  ~/{.bin,.config/composer/vendor/bin} /usr/local/go/bin ~/.gem/ruby/*/bin 2>/dev/null
 
 unset -f add-to-path
 
