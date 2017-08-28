e2964675fd3ed180294bced28058669d131b0919
nl253 <nl253@kent.ac.uk>
2017-08-28 19:17:42 +0100

    Update shell, bash and zsh configuration.

---
+++ .shells/packages.sh
@@ -0,0 +1,8 @@
+
+# PYTHON
+
+#for i in 'better_exceptions' 'numpy' 'pandas' 'ipdb' 'jedi'; do
+	#[[ ! -e ~/.local/lib/*/site-packages/*${i}* ]] && pip3 install --user --pre $i
+#done
+
+#[[ ! -e ~/.local/bin/vint ]] && pip install --user vim-vint
