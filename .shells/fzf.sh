192281b0d10bd8c65b7ff93c3816036477b693ae
nl253 <nl253@kent.ac.uk>
2017-09-03 19:30:36 +0100

    Quick commit. Nothing important ...

--- .shells/fzf.sh
+++ .shells/fzf.sh
@@ -6,6 +6,7 @@
 if [[ -x $(which fzf) ]]; then # {{{
 
   # KEYMAP
+  # ------------------------
   # enter : print to STDOUT
   # ctrl-d : scroll down
   # ctrl-u : scroll up
@@ -15,17 +16,29 @@ if [[ -x $(which fzf) ]]; then # {{{
   # type]
   # alt-l : open in `less`
 
-  export FZF_DEFAULT_OPTS="--bind='alt-d:execute(cd {})' --bind='ctrl-d:half-page-down,ctrl-u:half-page-up,alt-p:toggle-preview' --bind='alt-e:execute(\$EDITOR {})' --bind='alt-r:execute(rifle {}),alt-l:execute:($PAGER {})' --no-mouse --multi --black --margin 3% --prompt=' >> ' --reverse --tiebreak=end,length --color 'hl:117,hl+:1,bg+:232,fg:240,fg+:246'"
+  export FZF_DEFAULT_OPTS="\
+    --bind='alt-d:execute(cd {})' \
+    --bind='ctrl-d:half-page-down,ctrl-u:half-page-up,alt-p:toggle-preview' \
+    --bind='alt-e:execute(\$EDITOR {})' \
+    --bind='alt-r:execute(rifle {}),alt-l:execute:($PAGER {})' \
+    --no-mouse --multi --black --margin 3% --prompt=' >> ' \
+    --reverse --tiebreak=end,length --color 'hl:117,hl+:1,bg+:232,fg:240,fg+:246'"
 
-  export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD || find . -path "*/\.*" -prune -o -type d -print -type f -print -o -type l -print | sed s/^..//) 2> /dev/null'
+  export FZF_DEFAULT_COMMAND='(\
+    git ls-tree -r --name-only HEAD || \
+    find . -path "*/\.*" -prune -o -type d -print -type f -print -o -type l -print | sed s/^..//\
+    ) 2> /dev/null'
 
   export FZF_CTRL_T_OPTS="--select-1 --exit-0"
 
-  export FZF_CTRL_R_OPTS="--sort --exact --preview 'echo {}' --preview-window down:3:hidden --bind '?:toggle-preview'"
+  export FZF_CTRL_R_OPTS="\
+    --sort --exact --preview 'echo {}' \
+    --preview-window down:3:hidden --bind '?:toggle-preview'"
 
   # preview configured to `cat` for files and use `tree` for dirs
   # [FZF] with [P]REVIEW
-  alias fzfp='fzf --preview="[ -f {} ] && head -n 38 {} || tree -l -a --prune -L 4 -F --sort=mtime {}"'
+  alias fzfp='fzf --preview="[ -f {} ] && head -n 38 {} || \
+    tree -l -a --prune -L 4 -F --sort=mtime {}"'
 
   [[ -e ~/.gists ]] && alias gists='ls ~/.gists/*/* | fzfp'
 
