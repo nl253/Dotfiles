2988bbfd8a1149ece9b8b422f2741a2b33530a85
nl253 <norbertlogiewa96@gmail.com>
2017-10-13 17:46:13 +0100

    Quick commit. Nothing important ...

--- .cron/daily/pacman-save-installed.sh
+++ .cron/daily/pacman-save-installed.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# list installed pacman packages (-q without version info) (-e explicitly installed)
 if [[ -x $(command which pacman) ]]; then
   pacman -Qeq >~/.backup/pacman/installed.txt
 fi
