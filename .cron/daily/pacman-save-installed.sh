b43e8852b7ac08f184b1d307a28333877a896b2b
nl253 <nl253@kent.ac.uk>
2017-12-01 19:56:34 +0000

    Move cron-jobs to a submodule

--- .cron/daily/pacman-save-installed.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# list installed pacman packages (-q without version info) (-e explicitly installed)
-if [[ -x $(command which pacman) ]]; then
-  pacman -Qeq >~/.backup/pacman/installed.txt
-fi
--- .cron/hourly/remove-tags.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# remove ctags tag files (which by convention have the name `tags`)
-for i in find rm; do
-  [[ ! -x $(command which $i) ]] && return 0
-done
-
-find -type f -and -readable -and -name tags -exec rm {} +
